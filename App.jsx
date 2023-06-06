67d01d163350d8befb0d3838ba95fb2cfd54d893
fateme marashi <88906546+ftmemarashi@users.noreply.github.com>
2023-06-06 13:52:45 +0330

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+import Humbergermenu from "./components/Humbergermenu"
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+<Humbergermenu/>
+
+      </div>
+    )
+  }
+}
